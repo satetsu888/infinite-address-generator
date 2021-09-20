3d1edaf32a2d88910c88b4c15f788bf082773e5d
satetsu888 <satetsu888@gmail.com>
2021-09-20 14:07:50 +0900

    fix to registter onclick handler all time

--- src/background.ts
+++ src/background.ts
@@ -1,36 +1,34 @@
 import { buildUserNameContextFromTab, buildUserNameContextType } from './utils'
 import { generateAddress } from './domain/address'
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: 'root',
-        title: 'aaa',
-        contexts: ['editable']
-    })
+chrome.contextMenus.create({
+    id: 'root',
+    title: 'generate address',
+    contexts: ['editable']
+})
 
-    chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-        console.debug("context menu clicked")
-        if (!tab?.id) { return }
-        console.debug(tab)
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+    console.debug("context menu clicked")
+    if (!tab?.id) { return }
+    console.debug(tab)
 
-        // TODO: データがないときはオプション画面を案内する
-        const DEFAULT_USER_NAME_TEMPLATE = 'default-template'
-        const DEFAULT_DOMAIN = 'example.com'
-        const data = await ((): Promise<any> => {
-            return new Promise((resolve, reject) => {
-                chrome.storage.sync.get(['address.domain', 'address.usernameTemplate'], (data) => {
-                    resolve(data)
-                })
+    // TODO: データがないときはオプション画面を案内する
+    const DEFAULT_USER_NAME_TEMPLATE = 'default-template'
+    const DEFAULT_DOMAIN = 'example.com'
+    const data = await ((): Promise<any> => {
+        return new Promise((resolve, reject) => {
+            chrome.storage.sync.get(['address.domain', 'address.usernameTemplate'], (data) => {
+                resolve(data)
             })
-        })()
-        const usernameTemplate = typeof data['address.usernameTemplate'] === 'string' ? data['address.usernameTemplate'] : DEFAULT_USER_NAME_TEMPLATE
-        const domain = typeof data['address.domain'] === 'string' ? data['address.domain'] : DEFAULT_DOMAIN
+        })
+    })()
+    const usernameTemplate = typeof data['address.usernameTemplate'] === 'string' ? data['address.usernameTemplate'] : DEFAULT_USER_NAME_TEMPLATE
+    const domain = typeof data['address.domain'] === 'string' ? data['address.domain'] : DEFAULT_DOMAIN
 
-        const context: buildUserNameContextType = buildUserNameContextFromTab(tab)
+    const context: buildUserNameContextType = buildUserNameContextFromTab(tab)
 
-        const address: string = generateAddress(usernameTemplate, domain, context)
-        console.log(address)
+    const address: string = generateAddress(usernameTemplate, domain, context)
+    console.log(address)
 
-        chrome.tabs.sendMessage(tab.id, { action: 'paste_address', params: { address: address } })
-    })
+    chrome.tabs.sendMessage(tab.id, { action: 'paste_address', params: { address: address } })
 })
